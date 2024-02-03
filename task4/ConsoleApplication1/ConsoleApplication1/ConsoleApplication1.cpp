edf61e457952a6b21058ed3e9937643b4d785fc7
Svetakharsevich <123375587+Svetakharsevich@users.noreply.github.com>
2024-02-03 14:50:45 +0500

    Add files via upload

---
+++ task4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include<string>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int bal;
+    string name;
+    ifstream file("text.txt");
+
+    if (!file.is_open()) {
+        cout << "Файл невозможно открыть" << endl;
+    }
+    else {
+        while (getline(file, name)) {
+            cout << name << endl;
+        }
+        file.close(); // Закрываем файл после окончания чтения
+    }
+
+    return 0;
+}
